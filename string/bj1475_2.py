ef57fd03e6555127cee15ce6a0a7f75ee5fc1475
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-02-05 17:59:55 +0900

    [bj1475] 방 번호

---
+++ string/bj1475_2.py
@@ -0,0 +1,11 @@
+# 풀이2
+roomNum = input()
+numCnt = [0] * 9
+
+for n in range(len(roomNum)):
+    if (int(roomNum[n]) == 9):
+        numCnt[6] += 1
+    else:
+        numCnt[int(roomNum[n])] += 1
+        
+numCnt[6] = (numCnt[6] + 1) // 2
