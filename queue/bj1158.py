06acad903fadd3b70f0d73ed6fa11f61c8398e2d
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-02-07 16:18:23 +0900

    [bj1158] 요세푸스 문제

---
+++ queue/bj1158.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque(range(1, n + 1))
+res = []
+
+target = 1
+while(len(q) != 0):
+    if(target == k):
+        res.append(str(q.popleft())) # 출력 시 join 활용 위해 str 타입으로 변경
+        target = 1
+    else:
+        q.rotate(-1)
+        target += 1
+
+print("<{0}>".format(', '.join(res)))
