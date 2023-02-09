6f869fd74757895fa13112e5190289c36d7cf444
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-02-09 13:45:51 +0900

    [bj4673] 셀프 넘버

---
+++ bruteforce/bj4673.py
@@ -0,0 +1,13 @@
+arr = []
+num = 1
+
+while(num < 10000): # 100보다 작은 d(n) 전부 구하기
+    res = num
+    for i in str(num):
+        res += int(i)
+    arr.append(res)
+    num += 1
+
+for i in range(1, 10000 + 1): # 배열에 없는 값 출력
+    if(i not in arr):
+        print(i)
