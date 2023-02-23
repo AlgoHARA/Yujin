96ea82b908ace00cf84481369e90eb6f784418af
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-02-23 13:03:52 +0900

    주석 추가

--- dynamic/bj9095.py
+++ dynamic/bj9095.py
@@ -2,6 +2,7 @@ t = int(input())
 dp = [0, 1, 2, 4]
 
 for i in range(4, 11):
+    # dp[4] -> (dp[3] + 1) + (dp[2] + 2) + (dp[1] + 3)
     dp.append(dp[i - 1] + dp[i - 2] + dp[i - 3])
 
 for _ in range(t):
